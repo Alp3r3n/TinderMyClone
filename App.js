28fd777d2a07ee7c4369b62e846a865df1f8ca1e
Alperen Emir <alperenemirsoftware@gmail.com>
2023-05-27 02:13:27 +0300

    free

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React, {useEffect} from 'react';
-import {Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Card from './src/components/TinderCard';
 
 const App = () => {
   return (
     <View style = {styles.pageContainer}>
-      <View style = {styles.card}>
-          <ImageBackground source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'
-          }}
-            style = {styles.image}>
-                <View style = {styles.cardInner}>
-                  <Text style = {styles.name}>Elon Musk</Text>
-                  <Text style = {styles.bio}>A dude with a rocket is looking for a gal with fuel</Text>
-                </View>
-            </ImageBackground>
-        </View>  
+      <Card />
+      
       </View>
   ); 
 };
@@ -24,44 +16,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems : 'center',
     flex : 1,
-  },
-  card: {
-    width: '95%',
-    height : '70%',
-    borderRadius: 10,
-
-    shadowColor : '#000',
-    shadowOffsetOffest: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden', 
-
-    justifyContent: 'flex-end',
-    marginHorizontal: 10,
-  },
-  cardInner: {
-    padding: 10,
-  },
-  name : {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio : {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
-    marginHorizontal: 10,
   },
 });
 
